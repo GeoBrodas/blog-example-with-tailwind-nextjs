acb05612ae4b241ed1803b6d6c1e2185bdd56ae4
GeoBrodas <geobro2310@gmail.com>
2021-08-16 13:35:58 +0530

    add hero-image

--- components/Home-page/Hero/Hero.js
+++ components/Home-page/Hero/Hero.js
@@ -4,11 +4,13 @@ export default function Hero() {
   return (
     <section class="text-gray-600 body-font">
       <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img
+        <div class="lg:max-w-lg lg:w-full lg:pl-10 md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <Image
             class="object-cover object-center rounded"
-            alt="hero"
-            src="https://dummyimage.com/720x600"
+            alt="a prfile picture of Georgey"
+            src="/avataaars.png"
+            width={400}
+            height={450}
           />
         </div>
         <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
@@ -16,10 +18,9 @@ export default function Hero() {
             Hi there! I'm Georgey!
           </h1>
           <p class="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+            I'm Georgey, a Web Developer hailing from India! I love Web
+            development ever since I started learning it in the pandemic. I just
+            started to grow love in coding.
           </p>
         </div>
       </div>
